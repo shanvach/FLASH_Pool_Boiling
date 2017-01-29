744e77cfebe7231d920790cbf438b619f769b97c
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-01-28 23:00:44 -0500

    Test commit from splash

--- source/physics/Heat_AD/Heat_ADMain/vardens/gr_hypreCreateMatrix_KPD_FB.F90
+++ source/physics/Heat_AD/Heat_ADMain/vardens/gr_hypreCreateMatrix_KPD_FB.F90
@@ -360,9 +360,9 @@ subroutine gr_hypreCreateMatrix_KPD(iVar, bcTypes, bcValues, dt, &
 
               else  !- kpd - Boundary Node
 
-                 !MdensXL = 0.0 ! Neumann
+                 MdensXL = 0.0 ! Neumann
                  !MdensXL = 4.0
-                 MdensXL = 2*(facevarx(RH1F_FACE_VAR,i  ,j  ,k)+facevarx(RH2F_FACE_VAR,i  ,j  ,k)) ! Dirichlet
+                 !MdensXL = 2*(facevarx(RH1F_FACE_VAR,i  ,j  ,k)+facevarx(RH2F_FACE_VAR,i  ,j  ,k)) ! Dirichlet
                  temp_BoxVal(iter+1) = 0.0
 
               end if
@@ -401,9 +401,9 @@ subroutine gr_hypreCreateMatrix_KPD(iVar, bcTypes, bcValues, dt, &
 
               else  !- kpd - Boundary Node
 
-                 !MdensXR = 0.0 ! Neumann
+                 MdensXR = 0.0 ! Neumann
                  !MdensXR = 4.0
-                 MdensXR = 2*(facevarx(RH1F_FACE_VAR,i+1,j  ,k) + facevarx(RH2F_FACE_VAR,i+1,j  ,k)) ! Dirichlet
+                 !MdensXR = 2*(facevarx(RH1F_FACE_VAR,i+1,j  ,k) + facevarx(RH2F_FACE_VAR,i+1,j  ,k)) ! Dirichlet
                  temp_BoxVal(iter+2) = 0.0
 
               end if
@@ -451,9 +451,9 @@ subroutine gr_hypreCreateMatrix_KPD(iVar, bcTypes, bcValues, dt, &
 
               else   !- kpd - Boundary Node
 
-                 !MdensYL = 0.0 ! Neumann
+                 MdensYL = 0.0 ! Neumann
                  !MdensYL = 4.0
-                 MdensYL = 2*(facevary(RH1F_FACE_VAR,i  ,j,k)+facevary(RH2F_FACE_VAR,i  ,j  ,k)) ! Dirichlet
+                 !MdensYL = 2*(facevary(RH1F_FACE_VAR,i  ,j,k)+facevary(RH2F_FACE_VAR,i  ,j  ,k)) ! Dirichlet
                  temp_BoxVal(iter+3) = 0.0
 
               end if
