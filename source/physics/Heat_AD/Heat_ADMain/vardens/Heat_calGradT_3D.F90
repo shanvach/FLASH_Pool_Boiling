97188deae8c8e678c2750f90ba3e471c5145c76d
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-02-01 13:59:22 -0500

    3D code complete starting debugging

--- source/physics/Heat_AD/Heat_ADMain/vardens/Heat_calGradT_3D.F90
+++ source/physics/Heat_AD/Heat_ADMain/vardens/Heat_calGradT_3D.F90
@@ -11,15 +11,13 @@ subroutine Heat_calGradT_3D(Tnl,Tnv,T,s,pf,dx,dy,dz,ix1,ix2,jy1,jy2,kz1,kz2,nx,n
     real :: th,tol
     integer :: i,j,k
     real :: Tij,Tipj,Timj,Tijp,Tijm,dxp,dxm,dyp,dym,Tx,Ty,Tax,Tbx,Tay,Tby,dyc,dxc,Tpx,Tmx,Tpy,Tmy
+    real :: Tmz,Tpz,dzp,dzm,Tkm,Tkp,Tz
 
     tol = 0.01
 
-    k = 1
-
-    do i=ix1,ix2
-      do j=jy1,jy2
-
-!go to 100
+   do k=kz1,kz2
+    do j=jy1,jy2
+      do i=ix1,ix2
 
 !__________METHOD 1_____________!
 
@@ -46,9 +44,6 @@ subroutine Heat_calGradT_3D(Tnl,Tnv,T,s,pf,dx,dy,dz,ix1,ix2,jy1,jy2,kz1,kz2,nx,n
       
             if(th .lt. tol) then
 
-               !th = abs(s(i-1,j,k))/(abs(s(i-1,j,k))+abs(s(i+1,j,k)))
-               !Tpx = T(i-1,j,k)
-
                th = tol
 
             end if
@@ -66,16 +61,13 @@ subroutine Heat_calGradT_3D(Tnl,Tnv,T,s,pf,dx,dy,dz,ix1,ix2,jy1,jy2,kz1,kz2,nx,n
 
             if(th .lt. tol) then
 
-               !th = abs(s(i+1,j,k))/(abs(s(i+1,j,k))+abs(s(i-1,j,k)))
-               !Tmx = T(i+1,j,k)
-
                th = tol
 
             end if
 
             dxm = th*dx
             Timj = ht_Tsat
-
+         
          end if
 
          if(s(i,j,k)*s(i,j+1,k) .le. 0.) then
@@ -85,10 +77,7 @@ subroutine Heat_calGradT_3D(Tnl,Tnv,T,s,pf,dx,dy,dz,ix1,ix2,jy1,jy2,kz1,kz2,nx,n
             th = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j+1,k)))
 
             if(th .lt. tol) then
-
-                !th = abs(s(i,j-1,k))/(abs(s(i,j-1,k))+abs(s(i,j+1,k)))
-                !Tpy = T(i,j-1,k)
-                
+               
                 th = tol
 
             end if
@@ -106,9 +95,6 @@ subroutine Heat_calGradT_3D(Tnl,Tnv,T,s,pf,dx,dy,dz,ix1,ix2,jy1,jy2,kz1,kz2,nx,n
 
             if(th .lt. tol) then
 
-                !th = abs(s(i,j+1,k))/(abs(s(i,j+1,k))+abs(s(i,j-1,k)))
-                !Tmy = T(i,j+1,k)
-
                 th = tol
 
             end if
@@ -118,208 +104,56 @@ subroutine Heat_calGradT_3D(Tnl,Tnv,T,s,pf,dx,dy,dz,ix1,ix2,jy1,jy2,kz1,kz2,nx,n
 
          end if
 
+        if(s(i,j,k)*s(i,j,k+1) .le. 0.) then
+
+            Tpz = T(i,j,k)
+
+            th = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j,k+1)))
+
+            if(th .lt. tol) then
+               
+                th = tol
+
+            end if
+
+            dzp = th*dy
+            Tkp = ht_Tsat
+
+         end if
+
+         if(s(i,j,k)*s(i,j,k-1) .le. 0.) then
+
+            Tmz = T(i,j,k)
+
+            th = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j,k-1)))
+
+            if(th .lt. tol) then
+
+                th = tol
+
+            end if
+
+            dzm = th*dy
+            Tkm = ht_Tsat
+
+         end if
+
          Tx = ((-dxp**2)*(Timj) + (dxp**2)*(Tmx) - (dxm**2)*(Tpx) + (dxm**2)*(Tipj))/(dxp*dxm*(dxp+dxm))
          Ty = ((-dyp**2)*(Tijm) + (dyp**2)*(Tmy) - (dym**2)*(Tpy) + (dym**2)*(Tijp))/(dyp*dym*(dyp+dym))
-
-!100 continue
-
-go to 200
-
-!__________METHOD 2_____________!
-
-!___________X DIR_______________!
-
-         if(abs(s(i-1,j,k)) .le. abs(s(i+1,j,k))) then
-
-            if(s(i-1,j,k)*s(i,j,k) .le. 0.) then
-
-               if(abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i-1,j,k))) .gt. tol) then
-
-                  Tax = T(i,j,k)
-                  Tbx = ht_Tsat
-                  th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i-1,j,k))) 
-                  dxc = dx*th
-
-               else 
-          
-                  if(s(i+1,j,k)*s(i,j,k) .gt. 0) then
-                    
-                     Tax = T(i+1,j,k)
-                     Tbx = ht_Tsat
-                     th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i-1,j,k))) 
-                     dxc = dx*th + dx
-
-                  else
-             
-                     Tax = 0.
-                     Tbx = 0.
-                     dxc = 1.
-
-                  end if
-
-               end if
-
-            else
-
-               Tax = T(i,j,k)
-               Tbx = T(i-1,j,k)
-               dxc = dx
-
-               !Tax = 0.
-               !Tbx = 0.
-               !dxc = 1.
-
-            end if
-
-         else
-
-            if(s(i,j,k)*s(i+1,j,k) .le. 0.) then
-
-               if(abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i+1,j,k))) .gt. tol) then
-
-                  Tax = ht_Tsat
-                  Tbx = T(i,j,k)
-                  th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i+1,j,k)))
-                  dxc = dx*th
-
-               else
-
-                  if(s(i-1,j,k)*s(i,j,k) .gt. 0) then
-
-                     Tax = ht_Tsat
-                     Tbx = T(i-1,j,k)
-                     th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i+1,j,k)))
-                     dxc = dx*th + dx
-
-                  else
-
-                     Tax = 0.
-                     Tbx = 0.
-                     dxc = 1.
-
-                  end if
-
-               end if
-
-            else
-
-               Tax = T(i+1,j,k)
-               Tbx = T(i,j,k)
-               dxc = dx
-
-               !Tax = 0.
-               !Tbx = 0.
-               !dxc = 1.
-
-            end if
-
-         end if
-
-!___________Y DIR_______________!
-
-         if(abs(s(i,j-1,k)) .le. abs(s(i,j+1,k))) then
-
-            if(s(i,j-1,k)*s(i,j,k) .le. 0.) then
-
-               if(abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j-1,k))) .gt. tol) then
-
-                  Tay = T(i,j,k)
-                  Tby = ht_Tsat
-                  th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j-1,k)))
-                  dyc = dy*th
-
-               else
-
-                  if(s(i,j+1,k)*s(i,j,k) .gt. 0) then
-
-                     Tay = T(i,j+1,k)
-                     Tby = ht_Tsat
-                     th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j-1,k)))
-                     dyc = dy*th + dy
-
-                  else
-
-                     Tay = 0.
-                     Tby = 0.
-                     dyc = 1.
-
-                  end if
-
-               end if
-
-            else
-
-               Tay = T(i,j,k)
-               Tby = T(i,j-1,k)
-               dyc = dy
-
-               !Tay = 0.
-               !Tby = 0.
-               !dyc = 1.
-
-            end if
-
-         else
-
-            if(s(i,j,k)*s(i,j+1,k) .le. 0.) then
-
-               if(abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j+1,k))) .gt. tol) then
-
-                  Tay = ht_Tsat
-                  Tby = T(i,j,k)
-                  th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j+1,k)))
-                  dyc = dy*th
-
-               else
-
-                  if(s(i,j-1,k)*s(i,j,k) .gt. 0) then
-
-                     Tay = ht_Tsat
-                     Tby = T(i,j-1,k)
-                     th  = abs(s(i,j,k))/(abs(s(i,j,k))+abs(s(i,j+1,k)))
-                     dyc = dy*th + dy
-
-                  else
-
-                     Tay = 0.
-                     Tby = 0.
-                     dyc = 1.
-
-                  end if
-
-               end if
-
-            else
-
-               Tay = T(i,j+1,k)
-               Tby = T(i,j,k)
-               dyc = dy
-
-               !Tay = 0.
-               !Tby = 0.
-               !dyc = 1.
-
-            end if
-
-         end if
-
-
-         Tx = (Tax - Tbx)/dxc
-         Ty = (Tay - Tby)/dyc
-
-200 continue
+         Tz = ((-dzp**2)*(Tkm)  + (dzp**2)*(Tmz) - (dzm**2)*(Tpz) + (dzm**2)*(Tkp))/(dzp*dzm*(dzp+dzm))
 
          if (pf(i,j,k) .eq. 0.) then
           
-            Tnl(i,j,k) = + nx(i,j,k)*Tx + ny(i,j,k)*Ty 
+            Tnl(i,j,k) = + nx(i,j,k)*Tx + ny(i,j,k)*Ty + nz(i,j,k)*Tz
          
          else
          
-            Tnv(i,j,k) = - nx(i,j,k)*Tx - ny(i,j,k)*Ty
+            Tnv(i,j,k) = - nx(i,j,k)*Tx - ny(i,j,k)*Ty - nz(i,j,k)*Tz
          
          end if
                            
       end do
     end do
+  end do
 
 end subroutine Heat_calGradT_3D
