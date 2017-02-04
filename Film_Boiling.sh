77f5cf558a39c95128d3d9c1f1fce9a94dfc561a
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-02-04 05:40:17 -0500

    Commit from Splash

--- Film_Boiling.sh
+++ Film_Boiling.sh
@@ -1,1 +1,1 @@
-./setup INavierStokes/zoso/Film_Boiling  -2d -auto  -nxb=20 -nyb=20 -opt  -maxblocks=100 -gridinterpolation=native +pm4dev  -objdir=Film_Boiling -site=colonialone.gwu.edu Bittree=1
+./setup INavierStokes/zoso/Film_Boiling  -2d -auto  -nxb=20 -nyb=20 -opt  -maxblocks=100 -gridinterpolation=native +pm4dev  -objdir=Film_Boiling -site=splash.seas.gwu.edu Bittree=1
