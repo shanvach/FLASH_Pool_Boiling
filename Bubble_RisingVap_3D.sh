3e8303260332ee2e325295379d71d9e2c61089b4
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-02-16 23:06:10 -0500

    Adding Nucleate Boiling Test Case

--- Bubble_RisingVap_3D.sh
+++ Bubble_RisingVap_3D.sh
@@ -1,1 +1,1 @@
-./setup INavierStokes/zoso/Bubble_RisingVap_3D  -3d -auto  -nxb=20 -nyb=20 -nzb=20 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=Bubble_RisingVap_3D -site=colonialone.gwu.edu Bittree=1
+./setup INavierStokes/zoso/Bubble_RisingVap_3D  -3d -auto  -nxb=20 -nyb=20 -nzb=20 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=Bubble_RisingVap_3D -site=splash.seas.gwu.edu Bittree=1
