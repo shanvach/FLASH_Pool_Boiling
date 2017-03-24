dd1defd1d684c46438c2909375e1baaa937bda9c
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-03-24 12:28:20 -0500

    Increasing thickness of the smeared region

--- source/physics/Multiphase/MultiphaseMain/INStherm/mph_getSmearedProperties.F90
+++ source/physics/Multiphase/MultiphaseMain/INStherm/mph_getSmearedProperties.F90
@@ -19,7 +19,7 @@ subroutine mph_getSmearedProperties2D(s,pf,dx,dy,rho1,rho2,ix1,ix2,jy1,jy2,xnorm
         real, dimension(:,:,:), intent(inout) :: smhv,smrh
         INTEGER, parameter :: kz1 = 1
 
-         sp=1.d0*min(dx,dy)
+         sp=2.d0*min(dx,dy)
          smhv(ix1:ix2,jy1:jy2,kz1) = &
                  (1.d0 + derf(s(ix1:ix2,jy1:jy2,kz1)/sp))/2.d0
 
@@ -50,7 +50,7 @@ subroutine mph_getSmearedProperties3D(s,pf,dx,dy,dz,rho1,rho2,ix1,ix2,jy1,jy2,kz
         real :: sp
         real, dimension(:,:,:), intent(inout) :: smhv,smrh
 
-         sp=1.d0*min(dx,dy)
+         sp=2.d0*min(dx,dy)
          smhv(ix1:ix2,jy1:jy2,kz1:kz2) = &
                  (1.d0 + derf(s(ix1:ix2,jy1:jy2,kz1:kz2)/sp))/2.d0
 
