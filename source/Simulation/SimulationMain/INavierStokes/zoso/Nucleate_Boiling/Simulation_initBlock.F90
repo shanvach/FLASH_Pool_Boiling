32c9fad7e9239165c6c7a6e41375dd336849b85d
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-04-02 13:45:41 -0500

    Syncing all branches

--- source/Simulation/SimulationMain/INavierStokes/zoso/Nucleate_Boiling/Simulation_initBlock.F90
+++ source/Simulation/SimulationMain/INavierStokes/zoso/Nucleate_Boiling/Simulation_initBlock.F90
@@ -174,12 +174,12 @@ subroutine Simulation_initBlock(blockId)
            !r0 = 0.5d0
            !r0 = 5.0e-5
            !r0 = 0.02e-3
-           r0 = 0.1
+           r0 = 0.0033
            !x0 = 0.12d0
-           y0 = 0.1*cos((38.0/180.0)*acos(-1.0))
+           y0 = 0.0033*cos((30.0/180.0)*acos(-1.0))
 
            !x1 = -0.12d0
-           y1 = 0.1*cos((38.0/180.0)*acos(-1.0))
+           y1 = 0.0033*cos((30.0/180.0)*acos(-1.0))
 
            x0 =  0.3d0
            x1 =  0.6d0
@@ -249,7 +249,7 @@ subroutine Simulation_initBlock(blockId)
 
            !if(ycell .le. 0.2487) solnData(TEMP_VAR,i,j,k) = (0.2487-ycell)/0.2487
 
-           if(ycell .le. 0.2922) solnData(TEMP_VAR,i,j,k) = (0.2922-ycell)/0.2922
+           if(ycell .le. 0.0044) solnData(TEMP_VAR,i,j,k) = (0.0044-ycell)/0.0044
  
            !if(ycell .le. 0.2) solnData(TEMP_VAR,i,j,k) = (0.2 - ycell)/0.2
 
