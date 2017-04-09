29f3c5e65da3eae59bc76157be0599ae23fc6879
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-04-09 12:17:49 -0500

    Test 3

--- source/physics/Heat_AD/Heat_ADMain/vardens/Heat_extrapGradT.F90
+++ source/physics/Heat_AD/Heat_ADMain/vardens/Heat_extrapGradT.F90
@@ -28,10 +28,10 @@ subroutine Heat_extrapGradT(Tnl,Tnv,T,s,pf,dx,dy,dz,nx,ny,ix1,ix2,jy1,jy2,Tnl_re
 
    maxN = max(maxval(abs(nx)),maxval(abs(ny)))
 
-   !dt_ext = 0.5d0*(dx/maxN)
+   dt_ext = 0.5d0*(dx/maxN)
    !dt_ext = 1E-6
    !dt_ext = 0.5d0/(1.0d0/dx + 1.0d0/dy)
-   dt_ext = dx
+   !dt_ext = dx
 
    Tnl_i = Tnl
    Tnv_i = Tnv
