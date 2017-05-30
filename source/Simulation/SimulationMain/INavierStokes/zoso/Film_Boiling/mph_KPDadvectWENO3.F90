522e87043a589a885374da4b1db0ad3ea9f92d58
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-05-30 13:54:39 -0500

    commitment

--- source/Simulation/SimulationMain/INavierStokes/zoso/Film_Boiling/mph_KPDadvectWENO3.F90
+++ source/Simulation/SimulationMain/INavierStokes/zoso/Film_Boiling/mph_KPDadvectWENO3.F90
@@ -368,11 +368,21 @@
               !if (blockID .eq. 15 .AND. i.eq.8 .AND. j.eq.8) then
               !   print*,"Block15 8,8:",xcell,ycell,s(i,j,k),AA*(s(i,j,k)-ycell)
               !end if
+
+              if(ycell .gt. 31.0) then
+
+              s(i,j,k) = so(i,j,k) - dt*(frx*ur - flx*ul)/dx &
+                                   - dt*(fry*vr - fly*vl)/dy &
+                                   - ins_dampC*AA*(s(i,j,k)-ycell) &
+                                   + dt*10.88*(j-(481.0-12.0))/12.0
+
+              else
+
               s(i,j,k) = so(i,j,k) - dt*(frx*ur - flx*ul)/dx &
                                    - dt*(fry*vr - fly*vl)/dy &
                                    - ins_dampC*AA*(s(i,j,k)-ycell)
-                                   + dt*10.88*(j-(481-11))/11
-
+ 
+              end if
               !---------------------------------------------------------------
               !---------------------------------------------------------------
 
