f86708f98913f543fc374d217b323976b0288d9f
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-06-04 00:58:51 -0500

    Resetting stefan problem

--- Stefan_Problem.sh
+++ Stefan_Problem.sh
@@ -1,1 +1,1 @@
-./setup INavierStokes/zoso/Stefan_Problem  -2d -auto  -nxb=20 -nyb=20 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=Stefan_Problem -site=splash.seas.gwu.edu Bittree=1
+./setup INavierStokes/zoso/Stefan_Problem  -2d -auto  -nxb=10 -nyb=10 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=Stefan_Problem -site=splash.seas.gwu.edu Bittree=1
