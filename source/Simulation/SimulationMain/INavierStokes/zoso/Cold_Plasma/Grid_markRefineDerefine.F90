392f4e00a21bd9b44be059a034841281087aaf41
akashdhruv <akashdhruv@gwmail.gwu.edu>
2018-01-17 14:10:16 -0500

    changes refine derefine characteristics

--- source/Simulation/SimulationMain/INavierStokes/zoso/Cold_Plasma/Grid_markRefineDerefine.F90
+++ source/Simulation/SimulationMain/INavierStokes/zoso/Cold_Plasma/Grid_markRefineDerefine.F90
@@ -81,8 +81,8 @@ subroutine Grid_markRefineDerefine()
 
   !! index of the threshold var
   specs(1) = real(DELE_VAR)
-  specs(2) =  1e12  ! lower boundary of the refinement
-  specs(3) =  1e18  ! upper boundary of the refinement
+  specs(2) =  5.0e17  ! lower boundary of the refinement
+  specs(3) =  1.0e18  ! upper boundary of the refinement
   !specs(2) = -5.0d0
   !specs(3) =  0.0d0
 
