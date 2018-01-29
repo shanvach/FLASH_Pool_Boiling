982bc84d2a31b88ea05e79aa3d719d7c8288976d
lgmartinez <lgmartinez@splash.seas.gwu.edu>
2018-01-29 09:55:49 -0500

    LM: adding plasma supply rate

--- source/physics/Plasma/PlasmaMain/coldPlasma/Plasma_Solve.F90
+++ source/physics/Plasma/PlasmaMain/coldPlasma/Plasma_Solve.F90
@@ -1,10 +1,10 @@
-subroutine Plasma_Solve(T_p, T_gen, T_o, dfun, dcoeff, dt, dx, dy, ix1,ix2, jy1, jy2, T_res)
+subroutine Plasma_Solve(T_p, T_gen, T_o, T_source, dfun, dcoeff, dt, dx, dy, ix1,ix2, jy1, jy2, T_res)
 
 #include "Plasma.h"
 
   implicit none
   real, dimension(:,:,:), intent(inout) :: T_p
-  real, dimension(:,:,:), intent(in) :: T_o, T_gen, dfun, dcoeff
+  real, dimension(:,:,:), intent(in) :: T_o, T_source, T_gen, dfun, dcoeff
   real, intent(in) :: dt, dx, dy
   integer, intent(in) :: ix1, ix2, jy1, jy2
 
@@ -19,10 +19,10 @@ subroutine Plasma_Solve(T_p, T_gen, T_o, dfun, dcoeff, dt, dx, dy, ix1,ix2, jy1,
      !                        + 0.5*(1.0-sign(1.0,dfun(i,j,1)))*((dt*dcoeff(i,j,1))/(dy*dy))*(T_o(i,j+1,1)+T_o(i,j-1,1)-2.*T_o(i,j,1))&
      !                        + 0.5*(1.0-sign(1.0,dfun(i,j,1)))*(dt*T_gen(i,j,1))
 
-
-     T_p(i,j,1) = T_o(i,j,1) + (dt*T_gen(i,j,1)) & 
+     T_p(i,j,1) = T_o(i,j,1) + (dt*T_gen(i,j,1))   & 
+                             + (dt*T_source(i,j,1)) &
                              + ((dt*dcoeff(i,j,1))/(dx*dx))*(T_o(i+1,j,1)+T_o(i-1,j,1)-2.*T_o(i,j,1)) &
-                             + ((dt*dcoeff(i,j,1))/(dy*dy))*(T_o(i,j+1,1)+T_o(i,j-1,1)-2.*T_o(i,j,1)) 
+                             + ((dt*dcoeff(i,j,1))/(dy*dy))*(T_o(i,j+1,1)+T_o(i,j-1,1)-2.*T_o(i,j,1))  
 
      !set minimum value to avoid negative density
      if (T_p(i,j,1).le.1e3) then
