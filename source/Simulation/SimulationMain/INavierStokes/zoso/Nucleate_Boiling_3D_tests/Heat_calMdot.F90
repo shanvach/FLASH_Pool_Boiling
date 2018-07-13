6d6ea0661ad3f994c246ebd2d54c985c7d452c0a
akashdhruv <akashdhruv@gwmail.gwu.edu>
2018-07-12 20:56:40 -0400

    Changes to Heat_calMdot

--- source/Simulation/SimulationMain/INavierStokes/zoso/Nucleate_Boiling_3D_tests/Heat_calMdot.F90
+++ source/Simulation/SimulationMain/INavierStokes/zoso/Nucleate_Boiling_3D_tests/Heat_calMdot.F90
@@ -16,7 +16,9 @@ subroutine Heat_calMdot(mdot,Tnl,Tnv,alpha_l,alpha_v,nx,ny,ix1,ix2,jy1,jy2,kz1,k
      alpha = alpha_v/alpha_l 
 
      ! Mass Flux Equation
-     mdot(ix1:ix2,jy1:jy2,kz1:kz2) = SRP*(Tnl(ix1:ix2,jy1:jy2,kz1:kz2)+alpha*Tnv(ix1:ix2,jy1:jy2,kz1:kz2))
+     !mdot(ix1:ix2,jy1:jy2,kz1:kz2) = SRP*(Tnl(ix1:ix2,jy1:jy2,kz1:kz2)+alpha*Tnv(ix1:ix2,jy1:jy2,kz1:kz2))
+
+     mdot(ix1:ix2,jy1:jy2,kz1:kz2) = SRP*(Tnl(ix1:ix2,jy1:jy2,kz1:kz2))
 
      ! Constant Mass Flux
      !mdot(ix1:ix2,jy1:jy2,kz1:kz2) = -0.01
