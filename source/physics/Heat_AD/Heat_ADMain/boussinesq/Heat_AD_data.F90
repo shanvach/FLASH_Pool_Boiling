af8a83ef222f0fb0b3bc78d8ec5b76b9ef803a9a
aaron <aaron@splash.seas.gwu.edu>
2019-01-14 19:59:33 -0500

    begin Rayleigh-Benard with constdens modules

---
+++ source/physics/Heat_AD/Heat_ADMain/boussinesq/Heat_AD_data.F90
@@ -0,0 +1,9 @@
+module Heat_AD_data
+     
+     implicit none
+     real, save :: ht_Pr ! Prandtl Number
+     real, save :: ht_Nu ! Nusselt Number
+     real, save :: ht_Bi ! Biot Number
+     real, save :: ht_Twall_high, ht_Twall_low ! Non Dimensional wall Temp
+
+end module Heat_AD_data
