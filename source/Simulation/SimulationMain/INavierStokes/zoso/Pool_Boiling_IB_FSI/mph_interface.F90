59498eb127922ee8cde1c5ed291594d88ecb6532
akashdhruv <akashdhruv@gwmail.gwu.edu>
2019-03-12 20:45:47 -0400

    Pool boiling FSI setup

--- source/Simulation/SimulationMain/INavierStokes/zoso/Pool_Boiling_IB_FSI/mph_interface.F90
+++ source/Simulation/SimulationMain/INavierStokes/zoso/Pool_Boiling_IB_FSI/mph_interface.F90
@@ -48,13 +48,14 @@ Module mph_interface
 
 interface
         subroutine mph_KPDcurvature2DAB(s,crv,rho1x,rho2x,rho1y,rho2y,pf,w,sigx,sigy,dx,dy, &
-           rho1,rho2,xit,crmx,crmn,ix1,ix2,jy1,jy2,visc,vis1,vis2,alph,thco1,thco2,cp1,cp2,nrmx,nrmy,mflg,smhv,smrh)
+           rho1,rho2,xit,crmx,crmn,ix1,ix2,jy1,jy2,visc,vis1,vis2,alph,thco1,thco2,cp1,cp2,nrmx,nrmy,mflg,smhv,smrh,&
+           lambda)
         implicit none
         integer, intent(in) :: ix1,ix2,jy1,jy2
         real, intent(in) :: dx,dy,rho1,rho2,xit,vis1,vis2,thco1,thco2,cp1,cp2
         real, intent(out) :: crmx, crmn
 
-        real, dimension(:,:,:), intent(inout):: s,crv,rho1x,rho2x,rho1y, &
+        real, dimension(:,:,:), intent(inout):: lambda,s,crv,rho1x,rho2x,rho1y, &
                                                 rho2y,pf,w,sigx,sigy,visc,alph,nrmx,nrmy,mflg,smhv,smrh
         end subroutine mph_KPDcurvature2DAB
 end interface
@@ -207,11 +208,11 @@ interface
 end interface
 
 interface
-        subroutine mph_getSmearedProperties2D(s,pf,dx,dy,rho1,rho2,ix1,ix2,jy1,jy2,xnorm,ynorm,smhv,smrh)
+        subroutine mph_getSmearedProperties2D(s,pf,dx,dy,rho1,rho2,ix1,ix2,jy1,jy2,xnorm,ynorm,smhv,smrh,lambda)
 
         integer, intent(in) :: ix1,ix2,jy1,jy2
         real, intent(in) :: dx,dy,rho1,rho2
-        real, dimension(:,:,:), intent(in) :: xnorm,ynorm,s,pf
+        real, dimension(:,:,:), intent(in) :: xnorm,ynorm,s,pf,lambda
         real, dimension(:,:,:), intent(inout) :: smhv,smrh
 
         end subroutine
