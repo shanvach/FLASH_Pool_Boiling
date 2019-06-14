f38b3c6e92aee096038f3962d849df8c717f0b1e
aaron <aaron@splash.seas.gwu.edu>
2019-06-14 13:59:01 -0500

    not working ug

---
+++ Channel.sh
@@ -0,0 +1,1 @@
+./setup INavierStokes/zoso/Rayleigh_Benard/ -2d -auto -opt -nxb=16 -nyb=16 +pm4dev PfftSolver=HomBcTrigSolver -gridinterpolation=native -maxblocks=500 -objdir=CHN_2D -site=splash.seas.gwu.edu
