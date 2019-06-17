17ce0dd138de1e178b48e476771b519c5a1b7b03
akashdhruv <akashdhruv@gwmail.gwu.edu>
2019-06-17 14:24:03 -0500

    fixing droplet and flat plate IB problem

---
+++ DropletAndFlatPlate.sh
@@ -0,0 +1,1 @@
+./setup INavierStokes/zoso/DropletAndFlatPlate  -2d -auto  -nxb=20 -nyb=20 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=DropletAndFlatPlate -site=splash.seas.gwu.edu Bittree=1
