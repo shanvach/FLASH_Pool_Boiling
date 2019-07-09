bc4b3b09f4ed400eed691eb2715e281b1c1950f5
Elizabeth Gregorio <egregorio@gwu.edu>
2019-07-09 12:42:26 -0500

    setup directory and file for falling drop that hits a wall

---
+++ source/Simulation/SimulationMain/INavierStokes/zoso/FallingDropWithWall/FallingDropWithWall.sh
@@ -0,0 +1,1 @@
+./setup INavierStokes/zoso/FallingDropWithWall  -2d -auto  -nxb=20 -nyb=20 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=FALLINGDROPWITHWALL -site=splash.seas.gwu.edu Bittree=1
