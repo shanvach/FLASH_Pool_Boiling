881554588b19051003fd1a18613680dcf8fe197e
akashdhruv <akashdhruv@gwmail.gwu.edu>
2019-07-26 10:36:02 -0500

    changing impinging jet mph initialization

--- source/Simulation/SimulationMain/INavierStokes/zoso/Impinging_Jet_mph_IB/Simulation_initBlock.F90
+++ source/Simulation/SimulationMain/INavierStokes/zoso/Impinging_Jet_mph_IB/Simulation_initBlock.F90
@@ -168,7 +168,7 @@ subroutine Simulation_initBlock(blockId)
            !R_init = 0.5
            !R_init = 0.0 + 0.5*(ycell+sim_jet_depth)/sim_jet_depth
 
-           dfun_rect = -min(-sim_jet_depth-ycell, 21-xcell, xcell+21, ycell+79)
+           dfun_rect = -min(-sim_jet_depth-ycell-0.1, 21-xcell, xcell+21, ycell+79)
            solnData(DFUN_VAR,i,j,k)  = min(sqrt((xcell-0.0)**2+(zcell-0.0)**2)-R_init,dfun_rect)
            
            !solnData(DFUN_VAR,i,j,k)  = dfun_rect
