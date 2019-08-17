ff881d48d8fa3bc1e5ccb8516886de8a2d862819
aaron <aaron@splash.seas.gwu.edu>
2019-08-17 11:48:56 -0500

    stable for qual

--- ugRayleigh_Benard.sh
+++ ugRayleigh_Benard.sh
@@ -1,1 +1,1 @@
-./setup INavierStokes/zoso/Rayleigh_Benard/ -3d -auto -opt -nxb=20 -nyb=20 -nzb=80 +ug PfftSolver=DirectSolver -objdir=ugRBCAV_3D -site=splash.seas.gwu.edu
+./setup INavierStokes/zoso/Rayleigh_Benard/ -3d -auto -opt -nxb=12 -nyb=12 -nzb=48 +ug PfftSolver=DirectSolver -objdir=ugRBCAV_3D -site=splash.seas.gwu.edu
