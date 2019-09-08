54d2067772dfdec1f8fb790a963a6e6492aff481
Aaron Lentner <aaron.d.lentner@gmail.com>
2019-09-08 19:10:04 -0400

    base Rayleigh script

--- Rayleigh_Benard.sh
+++ Rayleigh_Benard.sh
@@ -1,1 +1,1 @@
-./setup INavierStokes/zoso/Rayleigh_Benard/ -2d -auto -opt -nxb=32 -nyb=32 +pm4dev -gridinterpolation=native -objdir=RBCAV_2D -site=splash.seas.gwu.edu
+./setup INavierStokes/zoso/Rayleigh_Benard/ -3d -auto -opt -nxb=42 -nyb=42 -nzb=252 +ug PfftSolver=DirectSolver -objdir=ugRBCAV_3D252 -site=manwe
--- ugRayleigh_Benard.sh
+++
@@ -1,1 +0,0 @@
-./setup INavierStokes/zoso/Rayleigh_Benard/ -3d -auto -opt -nxb=32 -nyb=64 -nzb=256 +ug PfftSolver=DirectSolver -objdir=ugRBCAV_3D256 -site=manwe
