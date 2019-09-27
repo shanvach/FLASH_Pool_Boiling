69f8808f7dcad73bd2d658e21d2df2aa71e149cb
akashdhruv <akashdhruv@gwmail.gwu.edu>
2019-09-27 16:25:43 -0500

    removing old boiling FSI setup

--- Pool_Boiling_IB_FSI.sh
+++
@@ -1,1 +0,0 @@
-./setup INavierStokes/zoso/Pool_Boiling_IB_FSI  -2d -auto -nxb=20 -nyb=20 -opt  -maxblocks=50 -gridinterpolation=native  +pm4dev -objdir=Pool_Boiling_IB_FSI -site=splash.seas.gwu.edu Bittree=1
--- source/Simulation/SimulationMain/INavierStokes/zoso/Pool_Boiling_IB_FSI/Pool_Boiling_IB_FSI.sh
+++
@@ -1,1 +0,0 @@
-./setup INavierStokes/zoso/Pool_Boiling_IB  -2d -auto  -nxb=20 -nyb=20 -opt  -maxblocks=10 -gridinterpolation=native +pm4dev  -objdir=Pool_Boiling_IB -site=colonialone.gwu.edu Bittree=1
