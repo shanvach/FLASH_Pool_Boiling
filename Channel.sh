534143d5d6a6d1879e2b91e91a3714906646405e
Aaron Lentner <aaron.d.lentner@gmail.com>
2019-12-15 00:48:18 -0500

    align to origin/master

--- Channel.sh
+++
@@ -1,1 +0,0 @@
-./setup INavierStokes/zoso/Rayleigh_Benard/ -2d -auto -opt -nxb=16 -nyb=16 +pm4dev PfftSolver=HomBcTrigSolver -gridinterpolation=native -maxblocks=500 -objdir=CHN_2D -site=splash.seas.gwu.edu
--- lidsetup_ug.sh
+++
@@ -1,1 +0,0 @@
-./setup INavierStokes/zoso/LidDrivenCavity/ -2d -auto -opt -nxb=12 -nyb=12 +ug -objdir=LIDCAV_UG -site=splash.seas.gwu.edu
