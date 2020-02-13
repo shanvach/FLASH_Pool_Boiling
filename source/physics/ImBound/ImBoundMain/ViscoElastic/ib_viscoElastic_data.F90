2a24e667be985583b98f7d46da6f16609d2664e0
akashdhruv <akashdhruv@gwmail.gwu.edu>
2020-02-13 03:22:26 -0500

    changes to visco elastic data file

---
+++ source/physics/ImBound/ImBoundMain/ViscoElastic/ib_viscoElastic_data.F90
@@ -0,0 +1,27 @@
+!! source/physics/ImBound/ViscoElastic/ib_viscoElastic_data
+!!
+!! NAME
+!!
+!!  ib_viscoElastic_data
+!!
+!!
+!! SYNOPSIS
+!!
+!!  MODULE ib_viscoElastic_data()
+!!
+!!
+!! ARGUMENTS
+!!
+!!
+!! DESCRIPTION
+!!
+!!  This stores data and limiter functions that are specific to the Visco Elastic sub unit
+!!
+!!***
+ 
+ 
+module ib_viscoElastic_data
+
+  real, save :: ib_vis_parameter
+
+end module ib_viscoElastic_data
