d71e70bf82fe885aa4b7ab05b8fdb175b6435e24
akashdhruv <akashdhruv@gwmail.gwu.edu>
2020-03-09 20:28:34 -0500

    setting up lid-driven cavity test

---
+++ visco_lidsetup.sh
@@ -0,0 +1,1 @@
+./setup INavierStokes/zoso/LidDrivenCavity_wSolid/ -2d -auto -opt -nxb=20 -nyb=20 -maxblocks=20 -gridinterpolation=native +pm4dev PfftSolver=HomBcTrigSolver -objdir=VISCO_LID -site=splash.seas.gwu.edu
